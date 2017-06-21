91864f7ae2693bda7a24628d4a365a456de0bc94
freenham <freenham@gmail.com>
2017-06-21 15:12:22 +0900

    포기한다 씨발백준년아

--- Backjoon/src/practice/P10828_스택.java
+++ Backjoon/src/practice/P10828_스택.java
@@ -1,5 +1,7 @@
 package practice;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class P10828_스택 {
@@ -9,9 +11,11 @@ public class P10828_스택 {
 	public P10828_스택(int size) {
 		stack = new int[size];
 	}
+
 	public void push(int x) {
 		stack[index++] = x;
 	}
+
 	public int pop() {
 		if (index <= 0) {
 			System.out.println("-1");
@@ -21,10 +25,12 @@ public class P10828_스택 {
 			return stack[index];
 		}
 	}
+
 	public int size() {
 		System.out.println(index);
 		return index;
 	}
+
 	public int empty() {
 		if (index == 0) {
 			System.out.println("1");
@@ -34,20 +40,26 @@ public class P10828_스택 {
 			return 0;
 		}
 	}
+
 	public int top() {
-		System.out.println(stack[index - 1]);
-		return stack[index - 1];
+		if (index == 0) {
+			System.out.println("-1");
+			return -1;
+		} else {
+			System.out.println(stack[index - 1]);
+			return stack[index - 1];
+		}
 	}
+
 	@SuppressWarnings("resource")
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
+	public static void main(String[] args) throws FileNotFoundException {
+//		Scanner s = new Scanner(System.in);
+		Scanner s = new Scanner(new FileInputStream("sample.txt"));
 		int n = s.nextInt();
-		int cnt=0;
+		int cnt = 0;
 		P10828_스택 stack = new P10828_스택(n);
-		while (cnt<n) {
+		while (cnt < n) {
 			String command = s.next();
-			if (command.equals("exit"))
-				break;
 			if (command.equals("push")) {
 				int x = s.nextInt();
 				stack.push(x);
